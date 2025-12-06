38ed67d390b2809c158ff57a10f26db228c57a1a
mumair-coders <m.umair@cybersoph.com>
2025-12-06 15:25:21 +0500

    setup routing

---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>This is Contact Page</div>;
+};
+
+export default page;
---
+++ app/home/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>This is Home Page</div>;
+};
+
+export default page;
