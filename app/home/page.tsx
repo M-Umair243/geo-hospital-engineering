5812ee66dc2e33de4d2279a7efe2d2e69ed5b402
mumair-coders <m.umair@cybersoph.com>
2025-12-06 19:56:55 +0500

    products section implement

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,11 @@
 import HeroCarousel from "@/components/hero-carousel";
+import ProductsCards from "./products-card";
 
 const page = () => {
   return (
     <div>
       <HeroCarousel />
+      <ProductsCards />
     </div>
   );
 };
